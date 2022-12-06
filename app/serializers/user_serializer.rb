400dd9bd7e9bc8ce3af48b7924c405c614f4da92
umar <mdumar0726@gmail.com>
2022-12-06 13:50:32 +0000

    config puma

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
-  attributes :id, :emp_id, :phone_number, :email, :designation, :user_roles
-=======
 
   attributes :id, :name, :emp_id, :phone_number, :email, :designation, :user_roles
->>>>>>> c88ccca4eb112620f3919b55407ded99f1096a69
   has_many :devices
   
   def user_roles
