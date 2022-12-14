ad9ffcfb3a0ad342bc7a2c3a8ee247edb646ece3
shiva Kumar <argulashiva529@gmail.com>
2022-12-14 16:03:43 +0530

    modified

--- spec/requests/sessions/users_signin_spec.rb
+++ spec/requests/sessions/users_signin_spec.rb
@@ -3,17 +3,18 @@ require "rails_helper"
 RSpec.describe "Sessions", type: :request do
   describe 'sessions /signin' do 
     before do
-    @user = FactoryBot.create(:user)
-    post '/users/signin', params: {
-    user: {
-      emp_id: @user.emp_id,
-      password: "shiva123"
-          }
-    }
-  end
+      @user = FactoryBot.create(:user)
+      post '/users/signin', params:
+      {
+        user:{ emp_id: @user.emp_id,password: "shiva123" }
+      }
+      @result= JSON.parse(response.body)
+      @token = @result['token']  
+    end
+
     it 'render the status code to success' do
-      byebug
       expect(response).to have_http_status(200)
     end
+
   end
 end
--- spec/requests/users/users_spec.rb
+++ spec/requests/users/users_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
+require 'requests/sessions/users_signin_spec.rb'
 
 RSpec.describe "Users", type: :request do
-  describe 'GET /index' do
-    let!(:user) { create(:user) }
-      it ' signing in user' do
-       post '/users/signin', params: {
-        user: {
-          emp_id:user.emp_id,
-          password: "shiva123" }
-        }
-      @result= JSON.parse(response.body)
-      @token = @result['token']
-      get '/users', headers:{ 'Authorization': @token } 
-    end
-    it 'returns all posts' do
-      expect(JSON(response.body).count).to eq(1)
-    end
-      it 'returns status code 200' do
-        expect(response).to have_http_status(200)
+  let!(:user) { create(:user) }
+    describe 'GET /index' do
+        it ' signing in user' do
+        post '/users/signin', params: {
+          user: {
+            emp_id:user.emp_id,
+            password: "shiva123" }
+          }
+        @result= JSON.parse(response.body)
+        @token = @result['token']
+        get '/users', headers:{ 'Authorization': @token } 
       end
-  end
+      it 'returns all posts' do
+        expect(JSON(response.body).count).to eq(1)
+      end
+        it 'returns status code 200' do
+          expect(response).to have_http_status(200)
+        end
+    end
 end
 
